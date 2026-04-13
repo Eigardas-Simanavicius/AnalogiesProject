322e195d73c324bf51d6961e93682f54e07a4bc3
Eigardas Simanavicius <eigardas24@gmail.com>
2026-04-13 11:26:50 +0100

    Working of reading all the analogies

--- src/main/java/org/main/AnalogyDataHolder.java
+++ src/main/java/org/main/AnalogyDataHolder.java
@@ -1,4 +1,52 @@
 package org.main;
+
+import org.main.Objects.RewriteRule;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.logging.Logger;
+
 // hold all the analogies, is incharge of parsing and what not
 public class AnalogyDataHolder {
+
+    ConcurrentHashMap<String,String> Analogies = new ConcurrentHashMap<>();
+    private static final Logger logger = Logger.getLogger(RewriteRule.class.getName());
+
+    public static void addAnalogiesFromFile(String filename) throws IOException {
+       int linesNumber = getLineNumber(filename);
+       System.out.println(linesNumber);
+       int sets = linesNumber/4;
+       System.out.println(sets + " " + sets * 4);
+    }
+
+    private static int getLineNumber(String filename){
+        int counter = 0;
+        try (BufferedReader br = new BufferedReader(new FileReader(filename))) {
+            while (br.readLine() != null) {
+                counter++;
+            }
+        } catch (IOException e) {
+            logger.warning("File not found, no rules will be written to");
+        }
+        return counter;
+    }
+
+
+    static class sortAnalogies extends Thread{
+        String filename;
+        int startLine;
+        int endline;
+        sortAnalogies(String filename,int startLine,int endline){
+
+        }
+
+
+    }
+
+
+
 }
--- src/main/java/org/main/Main.java
+++ src/main/java/org/main/Main.java
@@ -10,7 +10,7 @@ import java.util.logging.*;
 
 
 public class Main {
-    public static void main(String[] args) throws IllegalArgumentException {
+    public static void main(String[] args) throws IllegalArgumentException, IOException {
 
         //logger initialization
         Logger rootLogger = Logger.getLogger("");
@@ -36,36 +36,7 @@ public class Main {
             ConfigSetup.applyConfig(args[0]);
         }
 
-        System.out.println("Flat Abstract String");
-        System.out.println(AnalogyManager.convertToAbstractString(AnalogyManager.ConvertToOOP("(if (can (cause.0 *AIDS (some death (when crushing (crush something))))) (can (succeed_at *AIDS (crush something))))"),false));
-
-        System.out.println("\nPrettified Abstract String");
-        System.out.println(AnalogyManager.convertToAbstractString(AnalogyManager.ConvertToOOP("(if (can (cause.0 *AIDS (some death (when crushing (crush something))))) (can (succeed_at *AIDS (crush something))))"),true));
-
-
-        System.out.println("Converting to OOP and then back to String");
-        //System.out.println(AnalogyManager.ConvertToString(AnalogyManager.ConvertToOOP("(work in scientist (some lab (that (conduct experiment))))"),true));
-
-        // [original predicate]         {modifiers}[verbPredicate]_[prepositionPredicate]:[newArgument]{optional asterisk}&[byArgument]
-        RewriteRule rule1 = new RewriteRule("exercise","preform_of:exercise*&exercising");
-        RewriteRule rule2 = new RewriteRule("explode","destroy_of:explode*&exploding");
-        RewriteRule rule3 = new RewriteRule("explode","boom_of:boom*&booming");
-        RewriteRule rule4 = new RewriteRule("walk","run_of:sprint*&sprinting");
-        RewriteRule rule5 = new RewriteRule("walk","fly_with:fly*:flying");
-
-        rule4.rewrite(AnalogyManager.ConvertToOOP("(exercise *athlete muscles)"));
-
-        ArrayList<RewriteRule> rules = new ArrayList<>();
-        rules.add(rule1);
-        rules.add(rule2);
-        rules.add(rule3);
-        rules.add(rule4);
-        rules.add(rule5);
-        Clause testClause2 = (Clause)AnalogyManager.ConvertToOOP("(Sigma male (exercise.0 athelete muscle) (explode Gregs legs) (walk Steve road) (explode gregs feet))");
-        ArrayList<Predicate> ans = ReWriter.reWriteAnalogyAllPermuatations(rules,testClause2);
-        for (Predicate pred: ans){
-            System.out.println(pred.toString());
-        }
+        AnalogyDataHolder.addAnalogiesFromFile("/home/eigardas/Documents/Github/AnalogiesProject/structureddomains.txt");
 
     }
 }
